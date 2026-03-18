857a1c1cdd4129b89543f4bfe95561b437d37e4e
Kavia AI <production@kavia.ai>
2026-03-18 06:37:21 +0000

    Checkpoint 1 - cg69d595da

---
+++ .init/.linter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/iot-security-monitoring-dashboard-246158-246172/frontend_dashboard
+npm run build
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
